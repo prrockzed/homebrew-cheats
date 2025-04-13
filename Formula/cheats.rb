f6ff6201e27c5aa4e7af9d507460259e08e16c6d
prrockzed <priyanshuranjan2806@gmail.com>
2025-04-14 01:59:11 +0530

    Updated sha

--- Formula/cheats.rb
+++ Formula/cheats.rb
@@ -2,7 +2,7 @@ class Cheats < Formula
   desc "Simple terminal-based cheat sheet tool"
   homepage "https://github.com/prrockzed/cheats"
   url "https://github.com/prrockzed/cheats/archive/refs/heads/main.tar.gz"
-  sha256 "PASTE_YOUR_SHA_HERE"
+  sha256 "14ddd0f7e086d5dd361e273fd63258c85ad2e83bf393648f2900ddb4c5e71415"
   version "0.1.0"
 
   def install
